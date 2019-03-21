1d1909d356613f5d340ca0a1169b43febc8ac552
Habib Ridho <habib.ridho@amartha.com>
2019-03-21 22:14:43 +0700

    remove other test file

--- convey/calculation/sample_test.go
+++ convey/calculation/sample_test.go
@@ -32,3 +32,18 @@ func TestAdd(t *testing.T) {
 		})
 	})
 }
+
+func TestSub(t *testing.T) {
+	Convey("Given two values", t, func() {
+		x := 5
+		y := 2
+
+		Convey("When it is subtracted", func() {
+			result := Sub(x, y)
+
+			Convey("Then it should return 3", func() {
+				So(result, ShouldEqual, 3)
+			})
+		})
+	})
+}
